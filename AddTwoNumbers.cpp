e9946b614c3104076c2cb09c7b13014220c901a6
joye.chen <joye.chen@vivotek.com>
2018-05-09 20:56:18 +0800

    [A] 1.Add insertAtBeginning func, [M] 1.Support addTwoNumbers to handle linklist with two elements

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -12,29 +12,59 @@ struct ListNode {
 };
 
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
-    ListNode* res = new ListNode(0);
+    int sum = 0;
+    ListNode* res = NULL;
+    ListNode* head = NULL;
+
     while(l1 || l2){
-        res->val = l1->val + l2->val;
+        sum = l1->val + l2->val;
+        res = new ListNode(sum);
+        if(head == NULL)
+        {
+            head = res;
+        }
+        else
+        {
+            res->next = head;
+            head = res;
+        }
         if(l1) l1 = l1->next;
         if(l2) l2 = l2->next;
     }
     return res;
 }
 
+void insertAtBeginning(ListNode** head,int dataToBeInserted)
+{
+    ListNode *curr = new ListNode(dataToBeInserted);
+    
+    if(*head == NULL)
+    {
+        *head = curr;
+    }
+    else
+    {
+        curr->next = *head;
+        *head = curr;
+    }
 
+}
 
 int main() {
-    //2->4->3 + 5->6->4 = 7->0->8, only test 2 + 5 = 7
+    //2->4->3 + 5->6->4 = 7->0->8, only test 2 + 5 = 7, 4 + 6 = 10 (do not care carry)
     ListNode *current = NULL;
-    ListNode *first = new ListNode(2);
-    ListNode *second = new ListNode(5);
+    ListNode *first;
+    ListNode *second;
+    insertAtBeginning(&first, 2);
+    insertAtBeginning(&first, 4);
+    insertAtBeginning(&second, 5);
+    insertAtBeginning(&second, 6);
    
     current = addTwoNumbers(first, second);
     while (current != 0) {
-        std::cout << "val: " << current->val << " "; //val should be 7
+        std::cout << "val: " << current->val << " "; //val should be 7, 10
         current = current->next;
     }
     std::cout << std::endl;
     return 0;
 }
-

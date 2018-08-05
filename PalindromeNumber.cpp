4a776d1ddf6bed6b5d3d8dc47f57c21c360f195e
joye.chen <joye.chen@vivotek.com>
2018-08-05 13:00:40 +0800

    Check half of input by reversing from the end

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if(x < 0 || (x%10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = 10*revertedNumber + x%10;
+            x/=10;
+        }
+        return x==revertedNumber ||  x==revertedNumber/10;       
+    }
+};
